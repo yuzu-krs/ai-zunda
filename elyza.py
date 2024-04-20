6d8402eac534d521127c385676e5f79b2d4d0a5f
yuzu-krs <yztimai@icloud.com>
2024-04-20 12:03:51 +0900

    LLMモデルの実装

---
+++ elyza.py
@@ -0,0 +1,43 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+DEFAULT_SYSTEM_PROMPT = "一人称は「ぼく」、語尾に「なのだ」をつけてください"
+
+
+model_name = "elyza/ELYZA-japanese-Llama-2-7b-instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto")
+
+if torch.cuda.is_available():
+    model = model.to("cuda")
+
+while True:
+    start = time.time()
+    text=input("質問をどうぞ：")
+    prompt = "{bos_token}{b_inst} {system}{prompt} {e_inst} ".format(
+        bos_token=tokenizer.bos_token,
+        b_inst=B_INST,
+        system=f"{B_SYS}{DEFAULT_SYSTEM_PROMPT}{E_SYS}",
+        prompt=text,
+        e_inst=E_INST,
+    )
+
+
+    with torch.no_grad():
+        token_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+
+        output_ids = model.generate(
+            token_ids.to(model.device),
+            max_new_tokens=256,
+            pad_token_id=tokenizer.pad_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+    output = tokenizer.decode(output_ids.tolist()[0][token_ids.size(1) :], skip_special_tokens=True)
+    print(output)
+    end = time.time()
+    print(end-start)
+     
+

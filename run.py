010f4aa0c4c6781d359549d23a04cf56da6fa365
yuzu-krs <yztimai@icloud.com>
2024-04-24 02:27:30 +0900

    1sにした

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ aituber_system=AITuberSystem()
 while True:
     try:
         aituber_system.talk_with_comment()
-        time.sleep(5)
+        time.sleep(1)
     except Exception as e:
         print("エラーが発生しました")
         print(traceback.format_exc())
